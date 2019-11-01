e9c30f3fbfbb49b7334f775da2059ea74a9e80ff
Rahul chauhan <rahulchauhan@Rahuls-MacBook-Pro.local>
2019-11-01 23:43:23 +0530

    connect user information to database

--- TJ-ISF.js
+++ TJ-ISF.js
@@ -7,18 +7,52 @@ let scanner = new Instascan.Scanner({
 });
 scanner.addListener('scan', function(content) {
     scanner.stop()
+    document.getElementById("btnAgain").style.display = "block";
     document.getElementById("outdiv").style.display = "none";
-    document.getElementById("memberInfo").style.display = "block";
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var myArr = JSON.parse(this.responseText);
-            console.log(myArr)
-            document.getElementById("cardName").textContent = myArr.name;
+            if (myArr.authentication == "successful") {
+                document.getElementById('successMessage').style.display = "block"
+
+                document.getElementById("cardName").textContent = myArr.name;
+                document.getElementById("image").src = myArr.imageUrl;
+                document.getElementById('about').textContent = myArr.about;
+                document.getElementById('hobbies').textContent = myArr.hobbies
+                document.getElementById('skill0').textContent = myArr.skills[0]
+                document.getElementById('skill1').textContent = myArr.skills[1]
+                document.getElementById('skill2').textContent = myArr.skills[2]
+                document.getElementById('skill3').textContent = myArr.skills[3]
+                document.getElementById('twitter').onclick = () => { window.location.href = myArr.twitter; }
+                document.getElementById('linkedin').onclick = () => { window.location.href = myArr.linkedin; }
+                document.getElementById('twitter').onclick = () => { window.location.href = myArr.github; }
+                document.getElementById("memberInfo").style.display = "block";
+
+            } else {
+                document.getElementById("notFound").style.display = "block";
+            }
         }
     };
     xmlhttp.open("GET", url + content, true);
     xmlhttp.send();
 });
+
+document.getElementById("btnAgain").onclick = function() {
+    document.getElementById("btnAgain").style.display = "none";
+    document.getElementById("memberInfo").style.display = "none";
+    document.getElementById("notFound").style.display = "none";
+    Instascan.Camera.getCameras().then(function(cameras) {
+        if (cameras.length > 0) {
+            scanner.start(cameras[0]);
+            document.getElementById("outdiv").style.display = "block";
+        } else {
+            console.error('No cameras found.');
+        }
+    }).catch(function(e) {
+        console.error(e);
+    });
+}
+
 
 document.getElementById("btn").onclick = function() {
     document.getElementById("btn").style.display = "none";
